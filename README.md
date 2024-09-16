b5d518663715432ffdfeb333529802632914df60
sanjay-gobari <sanjay11gobari@gmail.com>
2024-09-16 13:51:22 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Guess the Flag Game
+
+## Code setup
+Step 1: download the file to your local machine.  
+Step 2: setup postgresSQL and import flag.csv (from inside public folder) file to your postgress database.  
+Step 3: open file with code editor.  
+Step 4: open terminal and run command.  
+```
+npm i
+```
+Step 5: Run Project
+```
+node .\index.js
+```
+```
+nodemon .\index.js
+```
+
---
+++ index.js
@@ -0,0 +1,68 @@
+import bodyParser from "body-parser";
+import express from "express"
+import pg from "pg"
+
+const app = express();
+const port = 3000;
+let quiz = []
+let current_quiz=null
+let score =0
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "Sanjay@#",
+    port: 5432
+})
+db.connect();
+db.query("SELECT * FROM flags lIMIT 2", (err, result) => {
+    if (!err) {
+        quiz = result.rows
+    }
+    else {
+        console.log("error in db", err)
+    }
+})
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function genques(){
+    current_quiz = quiz[getRandomNumber(0,quiz.length-1)]
+    console.log(current_quiz)
+}
+
+app.get("/", (req, res) => {
+    genques()
+    res.render("index.ejs", { data: {flag:current_quiz.flag,score} })
+
+})
+app.get("/retry", (req, res) => {
+    score=0;
+    res.redirect("/")
+
+})
+
+app.post("/submit", (req, res) => {
+    const udata = req.body;
+    console.log(udata)
+    if(udata.flag_name!='')
+    {
+        if(current_quiz.name.toLowerCase() == udata.flag_name.trim().toLowerCase()){
+            console.log("correct")
+            score +=1;
+            res.redirect("/")
+        }
+        else{
+            res.render("index.ejs", { data:{flag:null,score}})
+        }
+    }
+    else{
+        res.redirect("/")
+    }
+
+})
+
+app.listen(port, () => { console.log(`server running at http://localhost:${port}`) })
